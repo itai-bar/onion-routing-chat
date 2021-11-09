f418d3663205267cb09d27bb5c0d25f228e41e10
Tal <talbozorgi@gmail.com>
2021-11-10 01:25:50 +0200

    Edited documentation to be be uniformed

--- client/client.py
+++ client/client.py
@@ -11,23 +11,31 @@ def main():
 
 
 def send_message_and_get_response(sock_with_server, message_to_send):
+    """The function send message to the given socket and return the response
+
+    Args:
+        sock_with_server (socket): Socket to send it the message
+        message_to_send (string): Message to send to the server through the socket
+
+    Returns:
+        string.encode(): Response from the server
     """
-    The function send message to the given socket and return the response
-    :param sock_with_server: Socket to send it the message
-    :param message_to_send: Message to send to the server through the socket
-    :return: Response from the server
-    """
+
     sock_with_server.sendall(message_to_send.encode())
     return sock_with_server.recv(len(message_to_send))  # now we are dealing with echo server, going to be changed in the progress of the project
 
 
 def connect_to_server(ip, port):
+    """The function creates TCP socket, create connection with given 'ip' and 'port' and returns the connected socket
+
+    Args:
+        ip (string): Ip of server
+        port (int): Port to get his service
+
+    Returns:
+        socket: Socket connected to given ip and port
     """
-    The function creates TCP socket, create connection with given 'ip' and 'port' and returns the connected socket
-    :param ip: Ip of server
-    :param port: Port to get his service
-    :return: Socket connected to given ip and port
-    """
+    
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect the socket to the server
@@ -37,12 +45,12 @@ def connect_to_server(ip, port):
     return sock
 
 def get_nodes():
-    """get nodes from file
+    """Get nodes from file
 
     Returns:
-        list: ip's that stored in nodes.txt file
+        list: Ip's that stored in nodes.txt file
     """
-    
+
     curr_path = os.path.dirname(__file__)
     path_to_nodes = curr_path + r"\..\nodes.txt"
 
@@ -52,13 +60,13 @@ def get_nodes():
 
 
 def pad_ips(list_of_ips):
-    """this function get list of ip's not padding, for example 1.2.3.4 and return each padded 001.002.003.004
+    """This function get list of ip's not padding, for example 1.2.3.4 and return each padded 001.002.003.004
 
     Args:
-        list_of_ips (list): ip's to pad, each byte to fill with 000
+        list_of_ips (list): Ip's to pad, each byte to fill with 000
 
     Returns:
-        list: padded ip's as list
+        list: Padded ip's as list
     """
 
     padded_ip, padded_ips = [], []
@@ -74,4 +82,3 @@ def pad_ips(list_of_ips):
 
 if __name__ == "__main__":
     main()
-    print(pad_ips(get_nodes()))
