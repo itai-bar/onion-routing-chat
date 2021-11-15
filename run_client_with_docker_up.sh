4dbae42897ad1246ee04cf33cebcae7f5e290b85
Tal <talbozorgi@gmail.com>
2021-11-15 19:29:03 +0200

    Added script to print command to execute with correct order of ip's

---
+++ run_client_with_docker_up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NODES_CONTAINERS_IDS=$(docker ps | grep 'torbasedchat_node' | awk '{print $1;}')
+NODES_CONTAINERS_IPS=$(echo "$NODES_CONTAINERS_IDS" | xargs docker inspect | grep '"IPAddress"' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | tr "\n" " ")
+
+SERVER_CONTAINER_IP=$(docker ps | grep 'torbasedchat_chat_server' | awk '{print $1;}' | xargs docker inspect | grep '"IPAddress"' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+
+COMMAND_TO_EXECUTE="python client/client.py $NODES_CONTAINERS_IPS$SERVER_CONTAINER_IP"
+
+echo "$COMMAND_TO_EXECUTE"
