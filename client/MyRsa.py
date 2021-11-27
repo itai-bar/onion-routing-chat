ab62afc88a485134096e1a91961d686c79c12ded
Magshimim <talb8347@gmail.com>
2021-11-27 18:46:01 +0200

    added functions names without impelmentation

---
+++ client/MyRsa.py
@@ -0,0 +1,15 @@
+
+def generate_key_pair(bits):
+    pass
+def private_key_to_bytes(private_key):
+    pass
+def public_key_to_bytes(public_key):
+    pass
+def bytes_to_private_key(bytes):
+    pass
+def bytes_to_public_key(bytes):
+    pass
+def encrypt_with_public_key(msg, public_key):
+    pass
+def decrypt_with_private_key(ciphertext, private_key):
+    pass
