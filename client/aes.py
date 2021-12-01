c2c8272f48d735742a31c83495d655a98e2d873b
tal bozorgi <talb8348@gmail.com>
2021-12-01 21:43:14 +0200

    Removed base64 implementation, now using onlt bytes

--- client/aes.py
+++ client/aes.py
@@ -1,7 +1,5 @@
 from Crypto import Random
-from Crypto import Cipher
 from Crypto.Cipher import AES
-from base64 import b64encode, b64decode
 
 class Aes:
     def __init__(self, key=None, key_size=32):
@@ -10,14 +8,14 @@ class Aes:
         else:
             self._key = key
     
-    def encrypt(self, text: str) -> str:
-        """encrypted a given string using aes in CFB mode
+    def encrypt(self, text: bytes) -> bytes:
+        """encrypts a given text using aes in CFB mode
 
         Args:
-            text (str): aes encrypted string
+            text (bytes): aes encrypted text
 
         Returns:
-            str: encrypted and base64 encoded string
+            bytes: encrypted text
         """
         rem = len(text) % 16
         padded = str.encode(text) + (b'\0' * (16 - rem)) if rem > 0 else str.encode(text)
@@ -25,21 +23,20 @@ class Aes:
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self._key, AES.MODE_CFB, iv, segment_size=128)
         enc = cipher.encrypt(padded)[:len(text)]
-        return b64encode(iv + enc).decode()
+        return iv + enc
     
 
-    def decrypt(self, text: str) -> str:
-        """decrypts a message using aes in CFB mode and encoded with base64
+    def decrypt(self, text: bytes) -> bytes:
+        """decrypts a message using aes in CFB mode
 
         Args:
-            text (str): an encrypted message
+            text (bytes): an encrypted message
 
         Returns:
-            str: the original message
+            bytes: the original message
         """
-        text = b64decode(text) # the text was base64 encoded
         iv, value = text[:16], text[16:] # extracting the iv from the text
         rem = len(value) % 16
         padded = value + (b'\0' * (16 - rem)) if rem > 0 else value 
         cipher = AES.new(self._key, AES.MODE_CFB, iv, segment_size=128)
-        return cipher.decrypt(padded)[:len(value)].decode()
+        return cipher.decrypt(padded)[:len(value)]
--- pkg/tor_aes/tor_aes.go
+++ pkg/tor_aes/tor_aes.go
@@ -4,7 +4,6 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"
 	"errors"
 	"io"
 )
@@ -40,24 +39,24 @@ func NewAesGivenkey(givenKey []byte) *Aes {
 
 	plaintext []byte: plaintext
 */
-func (a *Aes) Encrypt(data string) (string, error) {
+func (a *Aes) Encrypt(data []byte) ([]byte, error) {
 	plaintext := []byte(data) // gotta work with bytes
 	block, err := aes.NewCipher(a.Key)
 	if err != nil {
-		return "", err
+		return []byte{}, err
 	}
 
 	// need to use a random iv for security
 	cipherText := make([]byte, aes.BlockSize+len(plaintext))
 	iv := cipherText[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return "", err
+		return []byte{}, err
 	}
 
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(cipherText[aes.BlockSize:], plaintext)
-	// encoding in base64 for easeier usage
-	return base64.StdEncoding.WithPadding(base64.StdPadding).EncodeToString(cipherText), nil
+
+	return cipherText, nil
 }
 
 /*
@@ -65,23 +64,19 @@ func (a *Aes) Encrypt(data string) (string, error) {
 
 	ciphertext []byte: encrypted text
 */
-func (a *Aes) Decrypt(cipherTextStr string) (string, error) {
-	cipherText, err := base64.StdEncoding.WithPadding(base64.StdPadding).DecodeString(cipherTextStr)
-	if err != nil {
-		return "", err
-	}
+func (a *Aes) Decrypt(cipherText []byte) ([]byte, error) {
 
 	block, err := aes.NewCipher(a.Key)
 	if err != nil {
-		return "", err
+		return []byte{}, err
 	}
 
 	if len(cipherText) < aes.BlockSize {
-		return "", errors.New("cipher text len is smaller than aes blocksize")
+		return []byte{}, errors.New("cipher text len is smaller than aes blocksize")
 	}
 
 	iv, cipherText := cipherText[:aes.BlockSize], cipherText[aes.BlockSize:]
 	stream := cipher.NewCFBDecrypter(block, iv)
 	stream.XORKeyStream(cipherText, cipherText)
-	return string(cipherText), nil
+	return cipherText, nil
 }
