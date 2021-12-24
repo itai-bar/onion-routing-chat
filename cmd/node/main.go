fc76312d9ee017ae53ab30b24163116817a1bd32
tal bozorgi <talb8348@gmail.com>
2021-12-24 13:54:01 +0200

    Added connection request to router

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	node.RunNode("0.0.0.0:8989")
+	//routerIp := "static-ip:7777" // TODO: change it to the static-ip of router when task is done
+	node.RunNode("0.0.0.0:8989") // TODO: remove this line when finishing with router static-ip
+	/*if node.NetworkLogon(routerIp){
+		log.Println("Performed 'Network-Logon' with tor-network.")
+		node.RunNode("0.0.0.0:8989")
+	}else{
+		log.Println("Couldn't perform 'Network-Logon' with tor-network.")
+	}*/ //TODO:change from comment to valid code when router-static ip is avialble
 }
--- node/node.go
+++ node/node.go
@@ -14,12 +14,45 @@ const (
 	CLOSE_SOCKET_SIZE      = 1
 	IP_SEGMENT_SIZE        = 15
 	DATA_SIZE_SEGMENT_SIZE = 5
+	CODE_NODE_CONN = "00"
 )
 
 type TorHeaders struct {
 	closeSocket int
 	nextIp      string
 	rest        []byte
+}
+
+
+/*
+	trying to perform with router, login to tor-network
+
+	routerAddress string: "ip:port"
+
+	return-value: true - connection agreed. false - connection refused.
+*/
+func NetworkLogon(routerAddress string) bool {
+	routerConn, err := net.Dial("tcp", routerAddress)
+	if err != nil {
+		log.Println("ERROR: ", err)
+		return false
+	}
+
+	routerConn.Write([]byte(CODE_NODE_CONN))
+	
+	respBuf := make([]byte, 1)
+	_, err = routerConn.Read(respBuf)
+	if err != nil {
+		return false
+	}
+
+	routerConn.Close()
+
+	passed := []byte("1") // "1" for true and performed logon succesfully
+	res := bytes.Compare(respBuf, passed)
+
+
+	return res==0 //if res==0-> connection passed. else-> connection refused
 }
 
 /*
