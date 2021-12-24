3793060ef7f83cd2e01776387761b70441f07a67
itai_bar <itaibar2004@gmail.com>
2021-12-24 15:27:40 +0200

    spllited the node files

---
+++ node/network.go
@@ -0,0 +1,56 @@
+package node
+
+import (
+	"bytes"
+	"log"
+	"net"
+)
+
+/*
+	trying to perform with router, login to tor-network
+
+	routerAddress string: "ip:port"
+
+	return-value: true - connection agreed. false - connection refused.
+*/
+func NetworkLogon(routerAddress string) bool {
+	routerConn, err := net.Dial("tcp", routerAddress)
+	if err != nil {
+		log.Println("ERROR: ", err)
+		return false
+	}
+
+	routerConn.Write([]byte(CODE_NODE_CONN))
+
+	respBuf := make([]byte, 1)
+	_, err = routerConn.Read(respBuf)
+	if err != nil {
+		return false
+	}
+
+	routerConn.Close()
+
+	passed := []byte("1") // "1" for true and performed logon succesfully
+	res := bytes.Compare(respBuf, passed)
+
+	return res == 0 //if res==0-> connection passed. else-> connection refused
+}
+
+/*
+	disconnect from tor-network
+
+	routerAddress string: "ip:port"
+*/
+func NetworkLogout(routerAddress string) {
+	routerConn, err := net.Dial("tcp", routerAddress)
+	if err != nil {
+		log.Println("ERROR: ", err)
+		return
+	}
+
+	routerConn.Write([]byte(CODE_NODE_DIS))
+
+	routerConn.Close()
+
+	log.Println("Logged out from tor-network")
+}
--- node/node.go
+++ node/node.go
@@ -14,65 +14,14 @@ const (
 	CLOSE_SOCKET_SIZE      = 1
 	IP_SEGMENT_SIZE        = 15
 	DATA_SIZE_SEGMENT_SIZE = 5
-	CODE_NODE_CONN = "00"
-	CODE_NODE_DIS  = "01"
+	CODE_NODE_CONN         = "00"
+	CODE_NODE_DIS          = "01"
 )
 
 type TorHeaders struct {
 	closeSocket int
 	nextIp      string
 	rest        []byte
-}
-
-
-/*
-	trying to perform with router, login to tor-network
-
-	routerAddress string: "ip:port"
-
-	return-value: true - connection agreed. false - connection refused.
-*/
-func NetworkLogon(routerAddress string) bool {
-	routerConn, err := net.Dial("tcp", routerAddress)
-	if err != nil {
-		log.Println("ERROR: ", err)
-		return false
-	}
-
-	routerConn.Write([]byte(CODE_NODE_CONN))
-	
-	respBuf := make([]byte, 1)
-	_, err = routerConn.Read(respBuf)
-	if err != nil {
-		return false
-	}
-
-	routerConn.Close()
-
-	passed := []byte("1") // "1" for true and performed logon succesfully
-	res := bytes.Compare(respBuf, passed)
-
-
-	return res==0 //if res==0-> connection passed. else-> connection refused
-}
-
-/*
-	disconnect from tor-network
-
-	routerAddress string: "ip:port"
-*/
-func NetworkLogout(routerAddress string) {
-	routerConn, err := net.Dial("tcp", routerAddress)
-	if err != nil {
-		log.Println("ERROR: ", err)
-		return
-	}
-
-	routerConn.Write([]byte(CODE_NODE_DIS))
-
-	routerConn.Close()
-
-	log.Println("Logged out from tor-network")
 }
 
 /*
@@ -288,7 +237,7 @@ func ExchangeKey(conn net.Conn) (*tor_aes.Aes, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	pemKey := make([]byte, length)
 	_, err = conn.Read(pemKey)
 	if err != nil {
