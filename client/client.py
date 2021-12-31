4908b0990a18cc27aa41df2ec7ddf0c8ef5d49a6
tal bozorgi <talb8348@gmail.com>
2021-12-31 12:15:18 +0200

    Added option to get list of ip's from router

--- client/client.py
+++ client/client.py
@@ -4,18 +4,22 @@ import key_exchange as ke
 import const
 import serialize
 import crypto
+import validation
 
 def main():
     message = ""
-    route_ips = get_nodes_and_dst_ips()  # [1st node, 2nd node, 3rd node, dst_ip]
+    if not validation.check_arguments_validation():
+        exit(0)
     rsa_key_pair = crypto.Rsa()  # creating Rsa class with random keypair for all sessions
-
-    while message != "Exit":
-        message = input("enter message: ")
-        if message == '':  # empty message is not allowed 
-            continue
-        resp = tor_message(message, route_ips, rsa_key_pair)
-        print(resp.decode())
+    route_ips = get_nodes_and_dst_ips(rsa_key_pair)  # [1st node, 2nd node, 3rd node, dst_ip]
+    print("got ip's succesfully:", route_ips)
+    
+    # while message != "Exit":
+    #     message = input("enter message: ")
+    #     if message == '':  # empty message is not allowed 
+    #         continue
+    #     resp = tor_message(message, route_ips, rsa_key_pair)
+    #     print(resp.decode())
 
 def tor_message(msg : str, route : list, rsa_key_pair : crypto.Rsa) -> bytes:
     """Sends a message using the tor protocol
@@ -61,22 +65,26 @@ def connect_to_server(ip : str, port : int) -> socket.socket:
     sock.connect(server_address)
     return sock
 
-def get_nodes_and_dst_ips() -> list:
-    """Get nodes ip's from argv
+def get_nodes_and_dst_ips(rsa_obj : crypto.Rsa) -> list:
+    """get router_ip and ip_of_destination from arguments and return nodes route and dst
 
     Returns:
-        list(string): Ip's that stored in nodes.txt file
+        list(string): Received ip's from router
     """
+    sock_with_router = connect_to_server(sys.argv[const.ROUTER_IP_IDX], const.ROUTER_PORT)
+    get_route_message = const.CODE_ROUTE + str(len(rsa_obj.pem_public_key)).zfill(const.MESSAGE_SIZE_LEN) + rsa_obj.pem_public_key.decode()
+    sock_with_router.sendall(get_route_message.encode())
 
-    nodes = []
-    for i in range(1,5):
-        try:
-            nodes.append(sys.argv[i])
-        except IndexError as e:
-            print("Please enter 4 ip's as arguments: client.py node_ip1 node_ip2 node_ip3 ip_of_destination")
-            exit(0)
-    return nodes
+    size = int(sock_with_router.recv(const.MESSAGE_SIZE_LEN).decode())
 
+    response = sock_with_router.recv(size)
+    sock_with_router.close()
+
+    list_of_ips = rsa_obj.decrypt(response).decode().split("&")
+    list_of_ips.append(sys.argv[const.DESTINATION__IP_IDX])
+    return list_of_ips
+ #  TODO: Change node that it will be able to connect to the tor-network using the router
+ #  TODO: Add router option to return encrypted ip's delimetered with &
 
 if __name__ == "__main__":
     main()
