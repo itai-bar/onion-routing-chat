a737e4be68af00bd50cbbba094b9cbdd1e85dad0
tal bozorgi <talb8348@gmail.com>
2021-12-31 10:30:28 +0200

    changed variable name to be more generic

--- client/key_exchange.py
+++ client/key_exchange.py
@@ -3,12 +3,12 @@ import socket
 import const
 from serialize import serialize_tor_message
 
-def key_exchange(ip_path : list, sock_with_first_node : socket.socket, rsa_obj : crypto.Rsa) -> list:
+def key_exchange(ip_path : list, sock_for_exchanging : socket.socket, rsa_obj : crypto.Rsa) -> list:
     """Function exchange aes keys using the given rsa object and returns the exchanged aes-keys as ordered(by ip's) list
 
     Args:
         ip_path (list): ip's for key exchange
-        sock_with_first_node (socket.socket): socket with first node
+        sock_for_exchanging (socket.socket): socket with first network component
         rsa_obj (crypto.Rsa): rsa private and public key as crypto.Rsa object
 
     Returns:
@@ -18,12 +18,12 @@ def key_exchange(ip_path : list, sock_with_first_node : socket.socket, rsa_obj :
     for idx, ip in enumerate(ip_path): # entering this after exchanged keys with first node
         message = serialize_tor_message(rsa_obj.pem_public_key.decode(), ip_path[1:idx+1], False, aes_keys)
 
-        sock_with_first_node.sendall(message)
+        sock_for_exchanging.sendall(message)
 
         # reading data len
-        size = int(sock_with_first_node.recv(const.MESSAGE_SIZE_LEN).decode())
+        size = int(sock_for_exchanging.recv(const.MESSAGE_SIZE_LEN).decode())
 
-        response = sock_with_first_node.recv(size)
+        response = sock_for_exchanging.recv(size)
         response = crypto.decrypt_by_order(response, aes_keys)
         aes_keys.append(crypto.Aes(rsa_obj.decrypt(response)))  # appending to aes_keys the aes key of curr iteration node
 
