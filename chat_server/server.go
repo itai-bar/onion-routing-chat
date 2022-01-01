59b0e79507ec9e4714c6c9e3ce9b7c73c3706f25
itai_bar <itaibar2004@gmail.com>
2022-01-01 02:16:39 +0200

    changed the chat servers code to work with the server package

--- chat_server/server.go
+++ chat_server/server.go
@@ -4,37 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strconv"
+	"torbasedchat/pkg/tor_server"
 )
 
 const (
 	DATA_SIZE_SEGMENT_SIZE = 5
 )
-
-/*
-	runs the server on a given address
-	calls HandleClient for a connected client
-
-	address string: "ip:port"
-*/
-func RunServer(address string) {
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatal("Error in listening:\t", err)
-	}
-	defer listener.Close() // will close the listener when the function exits
-	log.Println("Listening on:\t", address)
-
-	for {
-		conn, err := listener.Accept() // new client
-		if err != nil {
-			log.Fatal("Error on accepting client:\t", err)
-		}
-		log.Println("New client:\t", conn.RemoteAddr().String())
-
-		go HandleClient(conn) // new thread to handle the client
-	}
-}
 
 /*
 	handles the connection with every client
@@ -46,23 +21,11 @@ func HandleClient(conn net.Conn) {
 	defer conn.Close()
 
 	for {
-		dataSizeBuf := make([]byte, DATA_SIZE_SEGMENT_SIZE)
-		_, err := conn.Read(dataSizeBuf)
+		dataBuf, err := tor_server.ReadDataFromSizeHeader(conn, DATA_SIZE_SEGMENT_SIZE)
 		if err != nil {
 			continue
 		}
 
-		// data size is zero padded
-		dataSize, err := strconv.Atoi(string(RemoveLeadingChars(dataSizeBuf, '0')))
-		if err != nil {
-			continue
-		}
-
-		dataBuf := make([]byte, dataSize)
-		_, err = conn.Read(dataBuf)
-		if err != nil {
-			continue
-		}
 		log.Printf("Client %s: %s\n", conn.RemoteAddr().String(), string(dataBuf))
 
 		// zero padding the size again
@@ -73,16 +36,4 @@ func HandleClient(conn net.Conn) {
 			break
 		}
 	}
-}
-
-/*
-	removes every char c from the start of byte array s
-*/
-func RemoveLeadingChars(s []byte, c byte) []byte {
-	for i := range s {
-		if s[i] != c {
-			return s[i:]
-		}
-	}
-	return []byte{}
 }
