f287fda87b504bfc07f5c64a2cdc6af53b0144f2
itai_bar <itaibar2004@gmail.com>
2022-01-02 17:58:44 +0200

    changed the ip validatio to regex

--- client/validation.py
+++ client/validation.py
@@ -1,27 +1,20 @@
 import sys
+import re
 import const
 
 def check_arguments_validation():
     if len(sys.argv) != 3:
         print("Please enter 2 ip's as arguments: client.py [router_ip] [ip_of_destination]")
         return False
-    return validate_ip_address(sys.argv[const.ROUTER_IP_IDX]) and validate_ip_address(sys.argv[const.DESTINATION__IP_IDX])
+
+    ok = validate_ip_address(sys.argv[const.ROUTER_IP_IDX]) and validate_ip_address(sys.argv[const.DESTINATION__IP_IDX])
+
+    if ok:
+        print("IP addresses are valid")
+    else:
+        print("IP addresses are not valid")
+
+    return ok 
 
 def validate_ip_address(address):
-    parts = address.split(".")
-
-    if len(parts) != 4:
-        print("IP address {} is not valid".format(address))
-        return False
-
-    for part in parts:
-        if not part.isnumeric():
-            print("IP address {} is not valid".format(address))
-            return False
-
-        if int(part) < 0 or int(part) > 255:
-            print("IP address {} is not valid".format(address))
-            return False
- 
-    print("IP address {} is valid".format(address))
-    return True 
+    return re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", address)
