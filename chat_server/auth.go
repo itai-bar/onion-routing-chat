032187279700335a6f379f79e4c1ac3c01cf42a7
itai_bar <itaibar2004@gmail.com>
2022-01-11 22:23:08 +0200

    changed tor client to a simpler class, starts auth in server

---
+++ chat_server/auth.go
@@ -0,0 +1,83 @@
+package chat_server
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"net"
+	"torbasedchat/pkg/tor_aes"
+	"torbasedchat/pkg/tor_rsa"
+	"torbasedchat/pkg/tor_server"
+)
+
+const (
+	COOKIE_SIZE = 15
+)
+
+// used to create a more statefull connection with the clients
+type Cookie struct {
+	data [15]byte
+}
+
+/*
+	generates a random unique cookie to identify clients
+*/
+func CreateCookie() *Cookie {
+	var c Cookie
+	data := make([]byte, COOKIE_SIZE)
+
+	for {
+		rand.Read(data)
+
+		for i, data := range data {
+			c.data[i] = data
+		}
+		_, inMap := clients[c]
+
+		if !inMap {
+			break
+		}
+	}
+
+	return &c
+}
+
+/*
+	performs a key change with the client using a given RSA key
+	returns aes key to use with the client, cookie to identify a client and error
+*/
+func Auth(conn net.Conn) (*Cookie, *tor_aes.Aes, error) {
+	pemKey, err := tor_server.ReadDataFromSizeHeader(conn, DATA_SIZE_SEGMENT_SIZE)
+	if err != nil { // error can occur when router trying to check if node is alive
+		return nil, nil, err
+	}
+
+	log.Println(pemKey)
+
+	// inits a rsa object with the key we got from the client
+	// creating the aes key for the rest of comm
+	rsa, err := tor_rsa.NewRsaGivenPemPublicKey(pemKey)
+	aes := tor_aes.NewAesRandom()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	log.Println("got rsa key from client")
+
+	// adding the aes key and the cookie for the client
+	c := CreateCookie()
+
+	buf, err := rsa.Encrypt(append(c.data[:], aes.Key...))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// padding the length with to 5 chars
+	paddedLen := fmt.Sprintf("%05d", len(buf))
+	buf = append([]byte(paddedLen), buf...)
+
+	conn.Write(buf)
+
+	log.Println("sent rsa encrypted cookie + aes key")
+	return c, aes, nil
+}
--- client/main.py
+++ client/main.py
@@ -1,9 +1,26 @@
-from tor.client import tor_message
+from tor.client import TorClient
 from tor.crypto import Rsa
 
-if __name__ == '__main__':
-    rsa_key_pair = Rsa()  # creating Rsa class with random keypair for all sessions
-    resp = tor_message("hello", rsa_key_pair)
-    print(resp)
+import sys
 
-   
+
+# *** this is just a tester for the real client ***
+
+def serialize_msg(code, msg):
+    return code + str(len(msg)).zfill(5) + msg
+
+def deserialize_msg(msg):
+    return msg[4:]
+
+def auth(client : TorClient):
+    msg = serialize_msg('00', client._rsa.pem_public_key.decode())
+    print(f'sending {msg}')
+
+    resp = client.send(msg)
+    print(f'got: {client._rsa.decrypt(deserialize_msg(resp))}')
+
+
+if __name__ == '__main__':
+    client = TorClient(Rsa(), sys.argv[1], sys.argv[2])
+    auth(client)
+
